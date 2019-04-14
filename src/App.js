a3668bfe486fc2f671d06fb451515075d664066e
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-04-13 23:58:47 -0300

    organization

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App container">
+				<div className="options-box">
+					<h1>Find Your New NYC Home</h1>
+
+					<div>
+						<input id="show-listings" type="button" value="Show Listings" />
+						<input id="hide-listings" type="button" value="Hide Listings" />
+						<hr />
+						<span class="text"> Draw a shape to search within it for homes!</span>
+						<input id="toggle-drawing" type="button" value="Drawing Tools" />
+					</div>
+
+					<hr />
+
+					<div>
+						<input id="zoom-to-area-text" type="text" placeholder="Enter your favorite area!" />
+						<input id="zoom-to-area" type="button" value="Zoom" />
+					</div>
+
+					<hr />
+
+					<div>
+						<span className="text"> Within </span>
+
+						<select id="max-duration">
+							<option value="10">10 min</option>
+							<option value="15">15 min</option>
+							<option value="30">30 min</option>
+							<option value="60">1 hour</option>
+						</select>
+
+						<select id="mode">
+							<option value="DRIVING">drive</option>
+							<option value="WALKING">walk</option>
+							<option value="BICYCLING">bike</option>
+							<option value="TRANSIT">transit ride</option>
+						</select>
+
+						<span class="text">of</span>
+
+						<input id="search-within-time-text" type="text" placeholder="Ex: Google Office NYC or 75 9th Ave, New York, NY" />
+						<input id="search-within-time" type="button" value="Go" />
+					</div>
+
+					<hr />
+
+					<div>
+						<span className="text">Search for nearby places</span>
+						<input id="places-search" type="text" placeholder="Ex: Pizza delivery in NYC" />
+						<input id="go-places" type="button" value="Go" />
+					</div>
+
+				</div>
+
+				<div id="map"></div>
+			</div>
+		);
+	}
 }
 
 export default App;
